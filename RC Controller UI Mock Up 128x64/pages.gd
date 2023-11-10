ae12280f8ecaf7c50272d27279730c97a2ee5045
Ryan Mirch <rwmirch@gmail.com>
2023-11-10 02:25:23 -0500

    Page Changing System Working

--- RC Controller UI Mock Up 128x64/pages.gd
+++ RC Controller UI Mock Up 128x64/pages.gd
@@ -2,3 +2,22 @@ extends Node
 
 var MainPage = null
 var ControlsPage = null
+var MenuPage = null
+var ServoOffsetsPage = null
+var StatsPage = null
+var SettingsPage = null
+
+enum PageNames {Main, Controls, Menu, ServoOffsets, Stats, Settings}
+var pageDict = {}
+
+func _ready():
+	get_tree().create_timer(0.1).timeout.connect(updateDict)
+
+func updateDict():	
+	pageDict[PageNames.Main] = MainPage
+	pageDict[PageNames.Controls] = ControlsPage
+	pageDict[PageNames.Menu] = MenuPage
+	pageDict[PageNames.ServoOffsets] = ServoOffsetsPage
+	pageDict[PageNames.Stats] = StatsPage
+	pageDict[PageNames.Settings] = SettingsPage
+
